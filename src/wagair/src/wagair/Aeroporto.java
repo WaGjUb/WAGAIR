1520a7aab8e94faf36e122fc11026834363290d1
Wagjub <danielvalerio@alunos.utfpr.edu.br>
2016-05-05 15:38:15 -0300

    criação da classes aeroporto

---
+++ src/wagair/src/wagair/Aeroporto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wagair;
+
+/**
+ *
+ * @author a1625381
+ */
+public class Aeroporto {
+    String nome;
+    String cidade;
+    String pais;
+    
+    Aeroporto(String nome, String cidade, String pais)
+    {
+        this.nome = nome;
+        this.cidade = cidade;
+        this.pais = pais;
+    }
+}
